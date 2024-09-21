3fcb78a5f0f9592ffb3e8d4d112d91b669340f2d
Ali Akbar <akbarstudyinfo786@gmail.com>
2024-09-21 12:43:12 +0500

    Added web services

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,16 @@ import 'package:concierge_networking/manager/app_bloc_observer.dart';
 import 'package:concierge_networking/screens/onboarding/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = AppBlocObserver();
+  //  1 - Ensure firebase app is initialized if starting from background/terminated state
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const _App());
 }
 
